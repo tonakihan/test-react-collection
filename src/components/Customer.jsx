3b939eb7dc714b54ddd3ea06ff30d5e27cafbc11
tonakihan <tonakihan@gmail.com>
2024-02-06 16:02:10 +0900

    Fix

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 function Customer() {
   const dispath = useDispatch();
-  const customer = useSelector(state => state.customers.customer);
+  const customer = useSelector(state => state.customers.customers);
   
   function add() {
     dispath({type: 'ADD_CUSTOMER', payload: 1});
@@ -14,7 +14,10 @@ function Customer() {
   
   return(
     <div>
-      <h1>Customers: {customer}. Немного не works</h1>
+      {customer.length > 0 
+        ? <h1> Нету ни одного элемента</h1>
+        : <h1>Customers: {customer}. Немного не works</h1>
+      }
       <button onClick={() => add()}>Add</button>
       <button onClick={() => del()}>Del</button>
     </div>
